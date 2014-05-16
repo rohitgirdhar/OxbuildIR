bfa8ff349ff3963895504657657a9881ed164d1e
Rohit Girdhar <rohit@iiith>
2014-05-17 01:20:04 +0530

    more precompute in index

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Sample Usage
 ```bash
 $ cd src; make 
 # create inverted index
-$ ./buildIndex -d ../oxford_building_dataset/word_oxc1_hesaff_sift_16M_1M/ -o invIndex.txt -c invIndexFreq.txt
+$ ./buildIndex -d ../oxford_building_dataset/word_oxc1_hesaff_sift_16M_1M/ -o invIndex2.txt -c invIndexFreq2.txt -s imgStats.txt
 ```
--- src/buildIndex.cpp
+++ src/buildIndex.cpp
@@ -13,13 +13,13 @@ namespace po = boost::program_options;
 
 using namespace std;
 
-void addToIndex(set<int> descs, string img_idx, map<int, vector<string> > &invIdx) {
+void addToIndex(map<int,int> descs, string img_idx, map<int, map<string, int> > &invIdx) {
     for (auto iter = descs.begin(); iter != descs.end(); iter++) {
-        if (invIdx.count(*iter) <= 0) {
+        if (invIdx.count(iter->first) <= 0) {
             // not exists
-            invIdx[*iter] = vector<string>();
+            invIdx[iter->first] = map<string, int>();
         }
-        invIdx[*iter].push_back(img_idx);
+        invIdx[iter->first][img_idx] = iter->second;
     }
 }
 
@@ -29,7 +29,7 @@ void addToIndex(set<int> descs, string img_idx, map<int, vector<string> > &invId
  */
 void dumpToFile(string output_fpath,
         string output_idf_fpath,
-        map<int, vector<string> > invIdx) {
+        map<int, map<string, int> > invIdx) {
     ofstream fout, fout_idf;
     fout.open(output_fpath.c_str(), ios::out);
     fout_idf.open(output_idf_fpath.c_str(), ios::out);
@@ -39,12 +39,20 @@ void dumpToFile(string output_fpath,
         fout_idf << (iter->second).size() << endl; 
         for (auto iter2 = iter->second.begin(); 
                 iter2 != iter->second.end(); ++iter2) {
-            fout << *iter2 << " ";
+            fout << iter2->first << ":" << iter2->second << " ";
         }
         fout << endl;
     }
     fout.close();
     fout_idf.close();
+}
+
+void dumpToFileImgStats(string fname, map<string, pair<int,int> > img2total_max) {
+    ofstream fout(fname.c_str(), ios::out);
+    for (auto iter = img2total_max.begin(); iter != img2total_max.end(); ++iter) {
+        fout << iter->first << " " << (iter->second).first << " " << (iter->second).second << endl;
+    }
+    fout.close();
 }
 
 int main(int argc, char *argv[]) {
@@ -54,6 +62,7 @@ int main(int argc, char *argv[]) {
         ("dir,d", po::value<string>()->required(), "directory with oxc1 files")
         ("output,o", po::value<string>()->required(), "Output file")
         ("output-counts,c", po::value<string>()->required(), "Output IDF counts file")
+        ("output-img-stats,s", po::value<string>()->required(), "Output image name with total and max freq of descs")
         ;
 
     po::variables_map vm;
@@ -66,21 +75,31 @@ int main(int argc, char *argv[]) {
     }
     string dir = vm["dir"].as<string>();
     
-    map<int, vector<string> >invIdx;
+    map<int, map<string, int> > invIdx;
+    map<string, pair<int,int> > img2total_max;
     fs::recursive_directory_iterator rdi(dir), end;
     string img_fpath, img_fname;
     int count = 0;
     while (rdi != end) {
         img_fpath = (*rdi).path().string();
         img_fname = (*rdi).path().filename().string();
-        set<int> descs = readDescriptors(img_fpath);
+        map<int,int> descs = readDescriptorsWithCounts(img_fpath);
+        int total_descs = 0, max_descs = -1;
+        for(auto iter = descs.begin(); iter != descs.end(); ++iter) {
+            total_descs += iter->second;
+            max_descs = max(iter->second, max_descs);
+        }
         string img_idx = fpathToIdx(img_fname);
         addToIndex(descs, img_idx, invIdx);
+        img2total_max[img_idx] = make_pair(total_descs, max_descs);
         rdi++; count++;
         cout << "Done for " << img_fname << " (" << count << ")"<< endl;
     }
     dumpToFile(vm["output"].as<string>(),
             vm["output-counts"].as<string>(),
             invIdx);
+    dumpToFileImgStats(vm["output-img-stats"].as<string>(),
+            img2total_max);
+                
     return 0;
 }
