4280994d67b85f972021ccc1434c26a5d86b29b9
Rohit Girdhar <rohit@iiith>
2014-05-16 12:37:54 +0530

    basic image search

--- src/ImageSearch.m
+++ src/ImageSearch.m
@@ -1,13 +1,22 @@
-function ImageSearch(dpath, query_fpath, vocab_size)
-files = dir(fullfile(dpath, '*.txt'));
-nfiles = length(files);
-scores = zeros(1, nfiles);
+function ImageSearch(dpath, query_fpath, vocab_size, invIdxFPath)
 qvec = genVecFromImg(query_fpath, vocab_size);
-i = 1;
-for file = files'
-    fpath = fullfile(dpath, file.name);
+lines = textread(invIdxFPath, '%s', 'delimiter', '\n');
+relLines = lines(qvec(qvec > 0));
+clear lines;
+relImgs = [];
+for i = 1 : length(relLines)
+    imgIds = strsplit(relLines{i});
+    relImgs = union(relImgs, imgIds);
+end
+scores = zeros(1, numel(relImgs));
+i = 0;
+for relImg = relImgs'
+    i = i + 1;
+    if length(relImg{:}) == 0 % to ignore the '' string
+        continue;
+    end
+    fpath = fullfile(dpath, strcat(relImg{:}, '.txt'));
     tvec = genVecFromImg(fpath, vocab_size);
     scores(i) = dot(tvec, qvec);
-    i = i + 1
 end
 scores
