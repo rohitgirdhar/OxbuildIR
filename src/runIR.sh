76693c3e31d08070ce6c5329185cd2ae4bfee71b
Rohit Girdhar <rohit@iiith>
2014-05-16 19:32:48 +0530

    run IR script

---
+++ src/runIR.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ $# -lt 1 ]
+then
+    echo 'usage: ./run.sh <query img index>'
+    exit -1
+fi
+Q=$1
+tmpfile=`mktemp`
+./getClosest -d ../oxford_building_dataset/word_oxc1_hesaff_sift_16M_1M/ -i invIndex.txt  -k 10 -q $Q > $tmpfile
+bash matlab_batcher.sh RerankMatching \'../oxford_building_dataset/word_oxc1_hesaff_sift_16M_1M/\',\'../oxford_building_dataset/word_oxc1_hesaff_sift_16M_1M/${Q}.txt\',1000000,\'invIndex.txt\',\'invIndexFreq.txt\',\'${tmpfile}\',\'res.txt\'
+rm $tmpfile
