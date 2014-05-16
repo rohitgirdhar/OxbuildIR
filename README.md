666fa647aa6530536432ca4db7c25fa5a5510e89
Rohit Girdhar <rohit@iiith>
2014-05-16 12:44:52 +0530

    saving the IDFs

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Sample Usage
 ```bash
 $ cd src; make 
 # create inverted index
-$ ./buildIndex -d ../oxford_building_dataset/word_oxc1_hesaff_sift_16M_1M/ -o invIndex.txt
+$ ./buildIndex -d ../oxford_building_dataset/word_oxc1_hesaff_sift_16M_1M/ -o invIndex.txt -c invIndexFreq.txt
 ```
--- src/buildIndex.cpp
+++ src/buildIndex.cpp
@@ -47,19 +47,24 @@ void addToIndex(set<int> descs, string img_idx, map<int, vector<string> > &invId
  * Output format:
  * line i : [.. list of image IDs that have descriptor i..] 
  */
-void dumpToFile(string output_fpath, map<int, vector<string> > invIdx) {
-    ofstream fout;
+void dumpToFile(string output_fpath,
+        string output_idf_fpath,
+        map<int, vector<string> > invIdx) {
+    ofstream fout, fout_idf;
     fout.open(output_fpath.c_str(), ios::out);
+    fout_idf.open(output_idf_fpath.c_str(), ios::out);
     for (auto iter = invIdx.begin();
             iter != invIdx.end(); ++iter) {
         // fout << iter->first << " "; // not writing the word ID
+        fout_idf << (iter->second).size() << endl; 
         for (auto iter2 = iter->second.begin(); 
                 iter2 != iter->second.end(); ++iter2) {
-            fout << *iter2 << " "; 
+            fout << *iter2 << " ";
         }
         fout << endl;
     }
     fout.close();
+    fout_idf.close();
 }
 
 int main(int argc, char *argv[]) {
@@ -68,6 +73,7 @@ int main(int argc, char *argv[]) {
         ("help", "produce this help message")
         ("dir,d", po::value<string>()->required(), "directory with oxc1 files")
         ("output,o", po::value<string>()->required(), "Output file")
+        ("output-counts,c", po::value<string>()->required(), "Output IDF counts file")
         ;
 
     po::variables_map vm;
@@ -93,6 +99,8 @@ int main(int argc, char *argv[]) {
         rdi++; count++;
         cout << "Done for " << img_fname << " (" << count << ")"<< endl;
     }
-    dumpToFile(vm["output"].as<string>(), invIdx);
+    dumpToFile(vm["output"].as<string>(),
+            vm["output-counts"].as<string>(),
+            invIdx);
     return 0;
 }
