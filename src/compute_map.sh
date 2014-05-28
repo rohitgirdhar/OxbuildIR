6cd5377531903bce2860bbf1149192de221055fb
Rohit Girdhar <rohit@iiith>
2014-05-28 12:38:30 +0530

    advanced compute_map to fasten multiple K evalutaions

--- src/compute_map.sh
+++ src/compute_map.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-if [ $# -lt 4 ]
-then
-    echo    'Usage ./compute_map.sh <path to GT directory>' \
-            '<path to compute_map executable>'  \
-            '<input dir, with invIndex.txt etc>' \
-            '<K : precision at K. Use only with using compute_p. Otherwise (for AP) use #images>' \
-            '[<Path to file with names of selected images, to use with compute_ap> (optional)]'
-    exit -1
-fi
+HELP_TEXT="Usage ./compute_map.sh -g <path to GT directory>
+    -e <path to compute_map executable> 
+    -i <input dir, with invIndex.txt etc>
+    [-s <Path to file with names of selected images, to use with compute_ap> (optional)]
+    [<K : precision at K. Use only with using compute_p. Otherwise (for AP) use #images> ...] (multiple)"
 
-IP_DIR=$3
-COMPUTE_AP_EXEC=$2
-K=$4
-SEL_LIST=$5
+while getopts ":g:e:i:s:" opt; do
+    case "$opt" in 
+        g) GT_DIR=${OPTARG} ;;
+        e) COMPUTE_AP_EXEC=${OPTARG} ;;
+        i) IP_DIR=${OPTARG} ;;
+        s) SEL_LIST=${OPTARG} ;;
+        \?) echo $HELP_TEXT >&2
+            exit -1
+            ;;
+    esac
+done
+
+shift $(( OPTIND - 1 ))
+
+K=$@ # all the rest is K values
+if [ -z "$K" ]; then
+    K="5062"
+    echo "setting K=${K} since not supplied"
+fi
 TMP_FILE=`mktemp`
 TMP_Q_FILE=`mktemp`
 TMP_DIR=`mktemp -d`
@@ -21,7 +32,7 @@ tot=0
 count=0
 
 echo -n 'Making list of query in' $TMP_FILE '...'
-for file in `ls ${1}/*_query.txt`
+for file in `ls ${GT_DIR}/*_query.txt`
 do
     qimg_with_bounds=`cat $file`
     qimg=`echo $qimg_with_bounds | cut -f1 -d' '`
@@ -32,28 +43,45 @@ done
 echo 'Done'
 
 echo 'Writing output into' $TMP_DIR
-bash runIR_batch.sh $TMP_FILE $IP_DIR $TMP_DIR $K
+K_arr=($K)
+IFS=$'\n'
+MAX_K=`echo "${K_arr[*]}" | sort -nr | head -n1`
+echo "MAX K = " ${MAX_K} "out of ${K}"
+bash runIR_batch.sh $TMP_FILE $IP_DIR $TMP_DIR $MAX_K
 echo 'Done'
 
-echo '---AP values---'
-while read line && read -u 3 line2
-do
-    fbase=$line
-    qimg_with_bounds=$line2
-    qimg=`echo $qimg_with_bounds | cut -f1 -d' '`
-    ap_o=`${COMPUTE_AP_EXEC} ${1}/${fbase} ${TMP_DIR}/${qimg}.out ${SEL_LIST}`
-    ap=`echo ${ap_o} | sed -e 's/[eE]+*/\\*10\\^/'` # to handle scientific notation for bc
-    echo $qimg $ap_o '=' $ap
-    # IGNORE NAN values
-    if [ $ap = "-nan" ]
-    then
-        continue
-    fi
-    tot=$(bc <<< "scale=2;$tot+$ap")
-    count=`expr $count + 1`
-done < $TMP_Q_FILE 3< $TMP_FILE
+## CREATE the smaller files (_k)
+echo "Creating the smaller files for evaluation"
+for k in ${K_arr[@]}; do
+    while read line
+    do
+        qimg_with_bounds=$line
+        qimg=`echo $qimg_with_bounds | cut -f1 -d' '`
+        head -n${k} ${TMP_DIR}/${qimg}.out > ${TMP_DIR}/${qimg}_${k}.out
+    done < $TMP_FILE
+done
 
-echo -n 'mAP: '
-bc <<< "scale=2;$tot/$count"
+for k in ${K_arr[@]}; do
+    head -n${k} ${TMP_DIR}/${qimg}.out > ${TMP_DIR}/${qimg}_${k}.out
+    echo "---AP values (at K= ${k})---"
+    while read line && read -u 3 line2
+    do
+        fbase=$line
+        qimg_with_bounds=$line2
+        qimg=`echo $qimg_with_bounds | cut -f1 -d' '`
+        ap_o=`${COMPUTE_AP_EXEC} ${GT_DIR}/${fbase} ${TMP_DIR}/${qimg}_${k}.out ${SEL_LIST}`
+        ap=`echo ${ap_o} | sed -e 's/[eE]+*/\\*10\\^/'` # to handle scientific notation for bc
+        echo $qimg $ap_o '=' $ap
+        # IGNORE NAN values
+        if [ $ap = "-nan" ]
+        then
+            continue
+        fi
+        tot=$(bc <<< "scale=2;$tot+$ap")
+        count=`expr $count + 1`
+    done < $TMP_Q_FILE 3< $TMP_FILE
 
+    echo -n 'mAP: '
+    bc <<< "scale=2;$tot/$count"
+done
 #rm -rf $TMP_FILE $TMP_DIR $TMP_Q_FILE
