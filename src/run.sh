7011d7fb27c0b03f74ac80806937f7459a65b875
Rohit Girdhar <rohit@iiith>
2014-06-17 23:04:08 +0530

    run scr update

--- src/run.sh
+++ src/run.sh
@@ -11,4 +11,5 @@ bash createSubsetDir.sh ../oxford_building_dataset/word_oxc1_hesaff_sift_16M_1M/
 INDEX_DIR=indexes/`basename ${1} .txt`
 mkdir -p $INDEX_DIR
 ./buildIndex -o $INDEX_DIR -d subset_dir/
-bash  compute_map.sh -g ../oxford_building_dataset/gt/ -e ../oxford_building_dataset/compute_p -i $INDEX_DIR -s ${1} 1 3 5 10
+bash  compute_map.sh -g ../oxford_building_dataset/gt/ -e eval/compute_p -i $INDEX_DIR -s ${1} 1 3 5 10
+#bash  compute_map.sh -g ../oxford_building_dataset/gt/ -e ../oxford_building_dataset/compute_ap -i $INDEX_DIR -s ${1}
